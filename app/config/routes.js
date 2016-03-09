9f9c7ba38f8298a2c05ba47ab753a5c5aa3640cd
chekalin <stanislav.chekalin@gmail.com>
2016-03-09 06:39:51 +0000

    profiles page, component placeholders

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,9 +4,11 @@ var Home = require('../components/Home');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
+var Profile = require('../components/Profile')
 
 module.exports = (
     <Route path="/" component={Main}>
+        <Route path = "profile/:username" component={Profile} />
         <IndexRoute component={Home} />
     </Route>
 );
